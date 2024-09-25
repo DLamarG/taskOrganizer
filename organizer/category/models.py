5edb476f213c5f3039b8b45415ff5cc0cb81dbf7
DLamarG <demondg8@gmail.com>
2024-09-25 14:30:16 -0500

    added logic to all models for this project

--- organizer/category/models.py
+++ organizer/category/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+TITLE_CHOICES = {
+    "A": "Applications",
+    "N": "Networking",
+    "I": "Interviews",
+    "C": "Coding",
+    "P": "Projects",
+    "D": "Development",
+}
+
+# SHIRT_SIZES = {
+#         "S": "Small",
+#         "M": "Medium",
+#         "L": "Large",
+#     }
+
+
+class Category(models.Model):
+    category_title = models.CharField(max_length=1, choices=TITLE_CHOICES)
